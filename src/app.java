87f43b1eec621e29502a11a9d6d4402e2cd83332
viniciuslo66 <viniciuslo66@gmail.com>
2022-04-27 16:06:57 -0300

    Teste JAVAFX

--- src/app.java
+++ src/app.java
@@ -1,8 +1,32 @@
-/**
- * app
- */
-public class app {
-  public static void main(String[] args) {
-    System.out.println("aló mundo");
-  }
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+      primaryStage.setTitle("Teste");
+      Button button = new Button("Teste");
+      button.setOnAction(new EventHandler<ActionEvent>() {
+        
+        @Override
+        public void handle(ActionEvent arg0) {
+          System.out.println("Teste");
+        }
+        
+      });
+
+      StackPane root = new StackPane();
+      root.getChildren().addAll(button);
+      primaryStage.setScene(new Scene(root, 300, 200));
+      primaryStage.show();
+    }
 }
